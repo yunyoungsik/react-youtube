5d705e8f462d3f219c0ade66f90dda00eaa4d25a
yunyoungsik <yunyoungsik91@gmail.com>
2023-10-17 14:21:23 +0900

    페이지 만들기

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 
 ## 라이브러리 설치
 sass를 설치 : `npm install sass`
+react-router-dom 설치 : `npm install react-dom`
--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Today from './pages/Today'
+import Youtuber from './pages/Youtuber'
+import Not from './pages/Not'
 
-export const App = () => {
-    return (
-        <div>App</div>
-    )
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/today' element={<Today />} />
+        <Route path='/youtuber' element={<Youtuber />} />
+        <Route path='*' element={<Not />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
