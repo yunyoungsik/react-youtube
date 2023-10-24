c6ad04d5ae1d152099c8d5335d70eec612697883
yunyoungsik <yunyoungsik91@gmail.com>
2023-10-24 10:41:37 +0900

    2023.10.24

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -9,14 +9,17 @@ const Video = () => {
 
     useEffect(() => {
         fetchFromAPI(`videos?part=snippet,statistics&id=${videoId}`)
-            .then((data) => setVideoDetail(data.items[0]))
+            .then((data) => {
+                setVideoDetail(data.items[0])
+                // console.log(data)
+            })
     }, [videoId])
 
     return (
         <section id='videoView'>
             {videoDetail && (
-                <div className="video__view">
-                    <div className="video__play">
+                <div className='video__view'>
+                    <div className='video__play'>
                         <ReactPlayer
                             playing={true}
                             url={`https://www.youtube.com/watch?v=${videoId}`}
@@ -24,6 +27,17 @@ const Video = () => {
                             height='100%'
                             // style={{position: 'absolute', top:0, left:0}}
                         />
+                    </div>
+                    <div className='video__info'>
+                        <h2 className='video__title'>{videoDetail.snippet.title}</h2>
+                        <div className='video__channel'>
+                            <div className='id'>{videoDetail.snippet.channelId}</div>
+                            <div className='count'>
+                                <span className='view'>{videoDetail.statistics.viewCount}</span>
+                                <span className='like'>{videoDetail.statistics.likeCount}</span>
+                                <span className='comment'>{videoDetail.statistics.commentCount}</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             )}
